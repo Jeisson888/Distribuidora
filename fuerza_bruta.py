3886a4d80395e6fba2bc2fcb06b6b553a0be0c04
brayanfu038 <126002005+brayanfu038@users.noreply.github.com>
2023-11-20 09:40:53 -0500

    se agregaron comentarios

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,33 +1,36 @@
-from itertools import permutations
+from itertools import permutations  # Importa la función 'permutations' de la librería 'itertools'
 
 def fuerza_bruta(grafo, nodo_inicial):
-    nodos = list(grafo.nodos.keys())
-    nodos.remove(nodo_inicial)
+    nodos = list(grafo.nodos.keys())  # Obtiene una lista de nodos del grafo
+    nodos.remove(nodo_inicial)  # Remueve el nodo inicial de la lista de nodos
 
-    mejor_ruta = None
-    menor_peso = float('inf')
+    mejor_ruta = None  # Inicializa la mejor ruta encontrada como vacía
+    menor_peso = float('inf')  # Inicializa el peso de la ruta como infinito
 
+    # Genera todas las permutaciones posibles de los nodos restantes
     for perm in permutations(nodos):
-        ruta = [nodo_inicial] + list(perm)
-        peso_total = 0
-        ruta_valida = True
+        ruta = [nodo_inicial] + list(perm)  # Genera una ruta con el nodo inicial y la permutación actual
+        peso_total = 0  # Inicializa el peso total de la ruta actual como cero
+        ruta_valida = True  # Inicializa la bandera para verificar si la ruta es válida
 
+        # Calcula el peso total de la ruta y verifica su validez
         for i in range(len(ruta) - 1):
             nodo_actual = ruta[i]
             nodo_siguiente = ruta[i + 1]
 
-            arista = grafo.aristas.get((nodo_actual, nodo_siguiente))
+            arista = grafo.aristas.get((nodo_actual, nodo_siguiente))  # Obtiene la arista entre nodos
             if arista is None:
                 arista = grafo.aristas.get((nodo_siguiente, nodo_actual))
 
-            if arista is not None:
+            if arista is not None:  # Si la arista existe, suma su peso al peso total
                 peso_total += arista.calcular_peso()
-            else:
+            else:  # Si la arista no existe, marca la ruta como inválida y sale del bucle
                 ruta_valida = False
                 break
 
+        # Si la ruta es válida y su peso es menor al menor peso encontrado hasta ahora
         if ruta_valida and peso_total < menor_peso:
-            menor_peso = peso_total
-            mejor_ruta = ruta
+            menor_peso = peso_total  # Actualiza el menor peso encontrado
+            mejor_ruta = ruta  # Actualiza la mejor ruta encontrada
 
-    return mejor_ruta
+    return mejor_ruta  # Devuelve la mejor ruta encontrada
--- vecino_mas_cercano.py
+++ vecino_mas_cercano.py
@@ -1,22 +1,34 @@
 def vecino_mas_cercano(grafo, nodo_inicial):
+    # Conjunto de nodos que aún no han sido visitados.
     nodos_no_visitados = set(grafo.nodos.keys())
+    # Inicializa la ruta con el nodo inicial y lo remueve de los no visitados.
     ruta = [nodo_inicial]
     nodos_no_visitados.remove(nodo_inicial)
 
+    # Mientras haya nodos no visitados.
     while nodos_no_visitados:
+        # Selecciona el nodo actual de la ruta.
         nodo_actual = ruta[0]
+        # Inicializa el vecino más cercano y la distancia más corta.
         vecino_mas_cercano = None
         distancia_mas_corta = float('inf')
 
+        # Busca el vecino más cercano al nodo actual.
         for nodo in nodos_no_visitados:
+            # Busca la arista entre el nodo actual y el nodo actualmente revisado.
             arista = grafo.aristas.get((nodo_actual, nodo)) or grafo.aristas.get((nodo, nodo_actual))
             if arista:
+                # Calcula el peso de la arista.
                 peso = arista.calcular_peso()
+                # Si el peso es menor que la distancia más corta encontrada hasta ahora,
+                # actualiza la distancia más corta y el vecino más cercano.
                 if peso < distancia_mas_corta:
                     distancia_mas_corta = peso
                     vecino_mas_cercano = nodo
 
+        # Agrega el vecino más cercano a la ruta y lo elimina de los no visitados.
         ruta.append(vecino_mas_cercano)
         nodos_no_visitados.remove(vecino_mas_cercano)
 
+    # Retorna la ruta resultante.
     return ruta
